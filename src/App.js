3de280badf5a2ee06b5d5b4f74e27aabd9c6639b
jonas <pedrojonas.pj77@gmail.com>
2022-07-21 15:31:41 -0300

    Layout tela1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Tela1 from "./Tela1";
+
+function App(){
+    return(
+        <>
+        <Tela1/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/Tela1.js
@@ -0,0 +1,15 @@
+function Tela1(){
+    return(
+        <div className="initial">
+            <div>
+            <img className="logo1" src="/img/logo.png" />
+            ZapRecall
+            </div>
+            <div className="buttom">
+                Iniciar Recall!
+            </div>  
+        </div>
+    )
+}
+
+export default Tela1;
