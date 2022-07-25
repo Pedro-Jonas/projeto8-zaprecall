41c0ec5a88e8b08522435483fae331f2d025df5f
jonas <pedrojonas.pj77@gmail.com>
2022-07-25 15:42:13 -0300

    projeto concluido corrigido

--- src/Cards.js
+++ src/Cards.js
@@ -77,7 +77,7 @@ function Cards(props){
     {Q:"Componentes devem iniciar com __ " , R: "letra maiúscula"},
     {Q:"Podemos colocar __ dentro do JSX", R: "expressões"},
     {Q:"O ReactDOM nos ajuda __ ", R: "interagindo com a DOM para colocar componentes React na mesma"},
-    {Q:"Usamos o npm para __ ", R: " interagindo com a DOM para colocar componentes React na mesma"},
+    {Q:"Usamos o npm para __ ", R: "gerenciar os pacotes necessários e suas dependências"},
     {Q:"Usamos props para __ ", R: "passar diferentes informações para componentes"},
     {Q:"Usamos estado (state) para __ ", R: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
     ];
@@ -85,6 +85,8 @@ function Cards(props){
     function comparador() { 
         return Math.random() - 0.5; 
     };
+
+    console.log(questions)
 
     questions.sort(comparador)
     let Infocards = [
